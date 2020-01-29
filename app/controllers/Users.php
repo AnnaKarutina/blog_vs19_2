0ac584ff3fd2ec258b65f04669ccd96065b1d25a
Anna Karutina <anna.karutina@khk.ee>
2020-01-29 09:49:21 +0200

    User name validation

--- app/controllers/Users.php
+++ app/controllers/Users.php
@@ -13,6 +13,22 @@ class Users extends Controller
   }
 
   public function register(){
-    $this->view('users/register');
+    if($_SERVER['REQUEST_METHOD'] == 'POST'){
+//      (filter_input(INPUT_POST, FILTER_SANITIZE_STRING)); // to do
+      $data = array(
+        'name' => trim($_POST['name']),
+        'email' => trim($_POST['email']),
+        'password' => $_POST['password'],
+        'confirm_password' => $_POST['confirm_password'],
+        'name_err' => '',
+        'email_err' => '',
+        'password_err' => '',
+        'confirm_password_err' => ''
+      );
+      if(empty($data['name'])){
+        $data['name_err'] = 'Please enter the name';
+      }
+    }
+    $this->view('users/register', $data);
   }
 }
--- app/views/users/register.php
+++ app/views/users/register.php
@@ -10,22 +10,23 @@
         <p class="mt-2">Please fill the fields below to register</p>
       </div>
       <div class="card-body">
-        <form class="form" role="form">
+        <form class="form" role="form" method="post" action="<?php echo URLROOT.'/users/register';?>">
           <div class="form-group">
             <label for="name">Name<sup>*</sup></label>
-            <input type="text" class="form-control" id="name" placeholder="Full name">
+            <input type="text" class="form-control <?php echo (!empty($data['name_err'])) ? 'is-invalid' : ''; ?>" id="name" placeholder="Full name" name="name">
+            <?php echo (!empty($data['name_err'])) ? '<span class="invalid-feedback">'.$data['name_err'].'</span>' : '';?>
           </div>
           <div class="form-group">
             <label for="email">Email<sup>*</sup></label>
-            <input type="email" class="form-control" id="email" placeholder="Email">
+            <input type="email" class="form-control" id="email" placeholder="Email" name="email">
           </div>
           <div class="form-group">
             <label for="password">Password<sup>*</sup></label>
-            <input type="password" class="form-control" id="password" placeholder="Password">
+            <input type="password" class="form-control" id="password" placeholder="Password" name="password">
           </div>
           <div class="form-group">
             <label for="confirm_password">Verify password<sup>*</sup></label>
-            <input type="password" class="form-control" id="confirm_password" placeholder="Confirm Password">
+            <input type="password" class="form-control" id="confirm_password" placeholder="Confirm Password" name="confirm_password">
           </div>
 
           <div class="form-group">
